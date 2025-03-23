8be07f9353bbe069e0ca77a1368b524d6a68ebe4
Szell Katalin <kiskacc@yahoo.com>
2025-03-23 16:40:42 +0100

    Kommentek hozzaadasa by Kata

--- src/Fugorium_model/SporaFactory.java
+++ src/Fugorium_model/SporaFactory.java
@@ -1,6 +1,12 @@
 package Fugorium_model;
 
+/*
+ * SporaFactory osztaly ami a random sporaElement-ek letrehozasaert felel
+ */
 public class SporaFactory {
+    /*
+     * random sporaElement-ek letrehozasaert felelos metodus
+     */
     public Spora createRandomSpora() {
         System.out.println("SporaFactory createRandomSpora method called");
         return new BenitoSporaElement(); //csak pelda, kesobb majd random sporat ad vissza
--- src/Fugorium_model/VagastGatloSporaElement.java
+++ src/Fugorium_model/VagastGatloSporaElement.java
@@ -1,12 +1,23 @@
 package Fugorium_model;
 
+/**
+ * VagastGatloSporaElement osztaly, a Spora leszarmazottja
+ */
 public class VagastGatloSporaElement extends Spora {
 
+    /*
+     * Visitor design pattern reszekent, az accept metodus megvalositasa
+     */
     @Override
     public void accept(SporaVisitor visitor) {
         System.out.println("VagastGatloSporaElement accept method called");
     }
-
+    /*
+     * Visitor design pattern reszekent, az alkalmazHatast metodus megvalositasa
+     * @param Rovar peldany amire az allapotot alkalmazza a metodus
+     * @param RovarAllapot amilyen allapotot alklamaz a metodus a rovarra
+     * @param duration az allapot idotartama
+     */
     @Override
     public void alkalmazHatast(Rovar rovar, RovarAllapot allapot, int duration) {
         System.out.println("VagastGatloSporaElement alkalmazHatast method called");
