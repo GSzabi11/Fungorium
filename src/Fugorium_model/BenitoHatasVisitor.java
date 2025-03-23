fc612a2d2a511cbb70d976a260b4611ca4a1e314
Fedurcya Richárd <rfedurcya6@gmail.com>
2025-03-23 16:47:57 +0100

    Added Javadoc comments

--- src/Fugorium_model/BenitoHatasVisitor.java
+++ src/Fugorium_model/BenitoHatasVisitor.java
@@ -2,9 +2,17 @@ package Fugorium_model;
 
 public class BenitoHatasVisitor implements RovarVisitor{
 
+    /**
+     * Konstruktor
+     */
     public BenitoHatasVisitor(){
         System.out.println("BenitoHatasVisitor constructor");
     }
+
+    /** Implementálja a RovarVisitor függvényét és meglátogatja a paraméterként kapott rovaron
+     * @param rovar Ezen látogatja meg a RovarVisitor függvényét
+     */
+    @Override
     public void visit(Rovar rovar){
         System.out.println("BenitoHatasVisitor.visit()");
     }
--- src/Fugorium_model/LassitoHatasVisitor.java
+++ src/Fugorium_model/LassitoHatasVisitor.java
@@ -1,10 +1,18 @@
 package Fugorium_model;
 
-public class LassitoHatasVisitor {
+public class LassitoHatasVisitor implements RovarVisitor{
 
+    /**
+     * Konstruktor
+     */
     public LassitoHatasVisitor(){
         System.out.println("LassitoHatasVisitor constructor");
     }
+
+    /** Implementálja a RovarVisitor függvényét és meglátogatja a paraméterként kapott rovaron
+     * @param rovar Ezen látogatja meg a RovarVisitor függvényét
+     */
+    @Override
     public void visit(Rovar rovar){
         System.out.println("LassitoHatasVisitor.visit()");
     }
